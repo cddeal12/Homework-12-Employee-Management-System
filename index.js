1bf35ab3dc97834a43a59e5377538178fcf9996a
Nemancer <cddeal12@gmail.com>
2020-10-06 14:55:25 -0700

    Added addDepartment and addRole functions successfully

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: userName,
-    password: userPass,
-    database: ""
+    user: "root",
+    password: "Hardbut99?",
+    multipleStatements: true
 });
 
 // Creates the appropriate database and tables if they have not already been created
@@ -16,21 +16,21 @@ function initialize() {
     connection.query(
         `CREATE DATABASE IF NOT EXISTS employeeDB;
         USE employeeDB;
-
+        
         CREATE TABLE IF NOT EXISTS department (
             id INT AUTO_INCREMENT,
             name VARCHAR(30),
             PRIMARY KEY (id)
         );
-
+        
         CREATE TABLE IF NOT EXISTS role (
             id INT AUTO_INCREMENT,
             title VARCHAR(30),
             salary DECIMAL(10,2),
-            department_id INT,
+            department_id INT NULL,
             PRIMARY KEY (id)
-        )
-
+        );
+                
         CREATE TABLE IF NOT EXISTS employee (
             id INT AUTO_INCREMENT,
             first_name VARCHAR(30),
@@ -38,22 +38,24 @@ function initialize() {
             role_id INT,
             manager_id INT NULL,
             PRIMARY KEY (id)
-        )
-        `, function(err, res) {
+        );`, function(err, res) {
             if (err) throw err;
-            console.log("Welcome to the Employee Database.")
+            console.log("Welcome to the Employee Database.");
+            mainBranch();
         });
 }
 
-
 // Prompt Functions
+// =============================================================================================
+
+// Branch Prompts
 function mainBranch() {
     inquirer.prompt([
         {
             type: "list",
             message: "What will you do?",
             name: "mainChoice",
-            choices: ["Add to Database", "View Database", "Update Database"]
+            choices: ["Add to Database", "View Database", "Update Database", "Exit"]
         }
     ]).then(function(response) {
         if (response.mainChoice === "Add to Database") {
@@ -62,15 +64,93 @@ function mainBranch() {
             viewBranch();
         } else if (response.mainChoice === "Update Database") {
             updateBranch();
+        } else if (response.mainChoice === "Exit") {
+            connection.end();
         };
+    });
+};
+
+function addBranch() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What will you add to the database?",
+            name: "addChoice",
+            choices: ["Department", "Role", "Employee"]
+        }
+    ]).then(function(response) {
+        if (response.addChoice === "Department") {
+            addDepartment();
+        } else if (response.addChoice === "Role") {
+             addRole();
+        } else if (response.addChoice === "Employee") {
+            addEmployee();
+        }
+    });
+};
+
+
+// Add Prompts
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the new department?",
+            name: "newDept"
+        }
+    ]).then(function(response) {
+        connection.query(
+            `INSERT INTO department (name)
+            VALUES (?);`, response.newDept, function(err, res) {
+                if (err) throw err;
+                console.log(`Added new department ${response.newDept} successfully.`);
+                mainBranch();
+            });
+    });
+};
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of the new role?",
+            name: "newRole"
+        },
+        {
+            type: "input",
+            message: "What is the salary of the new role?",
+            name: "newRoleSalary"
+        },
+        {
+            type: "input",
+            message: "What department does the new role belong to? (Will be 'null' if no matching department exists)",
+            name: "newRoleDept"
+        }
+    ]).then(function(response) {
+
+        // Finds the matching ID of the provided department name
+        var newRoleDeptId;
+        connection.query("SELECT * FROM department WHERE name = ?", response.newRoleDept, function(err, res) {
+            if (err) throw err;
+            console.log(res);
+            if (res.length>0) {newRoleDeptId = res[0].id};
+            console.log(newRoleDeptId);
+            // Inserts new Role Including the corresponding department ID
+            connection.query(
+                `INSERT INTO role (title, salary, department_id)
+                VALUES ('${response.newRole}', '${response.newRoleSalary}', '${newRoleDeptId}');`, function(err, res) {
+                    if (err) throw err;
+                    console.log(`Added new role ${response.newRole} successfully.`);
+                    mainBranch();
+            });
+        });
+        
+        
     });
 };
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    initialize();
 });
-
-initialize();
-
-mainBranch();
